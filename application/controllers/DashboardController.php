adb7373280954130618374cc5859df1130ab03f6
rendisimamora <rendisimamora7127@gmail.com>
2018-04-05 19:45:42 +0000

    Fix from server

--- application/controllers/DashboardController.php
+++ application/controllers/DashboardController.php
@@ -21,7 +21,8 @@ class DashboardController extends CI_Controller {
 
 	public function __construct()
 	{
-		parent::__construct();
+		parent::__construct();	
+		$this->load->library('email');
 		if($this->session->userdata('username') == ""){
 			redirect('trisakti_logadmin');
 			
@@ -835,19 +836,211 @@ class DashboardController extends CI_Controller {
 		$this->load->view('back-end/footer/datatables_footer');
 	}
 
-	public function verifyUsers() {
-		$id = $this->uri->segment(2);
-		$data = array(
-			'verified' => '1'
+public function verifyUsers() {
+	$id = $this->uri->segment(2);
+    $query = $this->db->query("SELECT * FROM users WHERE id = '$id'");
+    $row = $query->row();
+    $first = $row->name;
+    $x =  ucfirst($first);
+    $this->email->from('trisakticonnect@gmail.com','Aktifasi Akun'); 
+    $this->email->to($row->email); 
+    $this->email->subject('Aktifasi Akun'); 
+		echo $this->email->message("
+		<html xmlns='http://www.w3.org/1999/xhtml'>
+      <head>
+      <meta http-equiv='Content-Type' content='text/html; charset=utf-8' />
+      <title>Aktifasi Akun</title>
+      <style type='text/css'>
+        body {padding-top: 0 !important;padding-bottom: 0 !important;padding-top: 0 !important;padding-bottom: 0 !important;margin:0 !important;width: 100% !important;
+        -webkit-text-size-adjust: 100% !important;-ms-text-size-adjust: 100% !important;-webkit-font-smoothing: antialiased !important;}
+        .tableContent img {border: 0 !important;display: block !important;outline: none !important;}a{color:#382F2E;}
+        p, h1{color:#382F2E;margin:0;}p{text-align:left;color:#999999;font-size:14px;font-weight:normal;line-height:19px;}
+        a.link1{color:#382F2E;}a.link2{font-size:16px;text-decoration:none;color:#ffffff;}h2{text-align:left;color:#222222; font-size:19px;font-weight:normal;}div,p,ul,h1{
+        margin:0;}.bgBody{background: #ffffff;}.bgItem{background: #ffffff;}@media only screen and (max-width:480px){table[class='MainContainer'], td[class='cell'] 
+        {width: 100% !important;height:auto !important;}td[class='specbundle']{width:100% !important;float:left !important;font-size:13px !important;
+        line-height:17px !important;display:block !important;padding-bottom:15px !important;}td[class='spechide']{
+        display:none !important;}img[class='banner']{width: 100% !important;height: auto !important;}td[class='left_pad'] {padding-left:15px !important;padding-right:15px !important;}}
+        @media only screen and (max-width:540px){table[class='MainContainer'], td[class='cell']{width: 100% !important;height:auto !important; }td[class='specbundle'] {
+        width:100% !important;float:left !important;font-size:13px !important;line-height:17px !important;display:block !important;padding-bottom:15px !important;}
+        td[class='spechide']{display:none !important;}img[class='banner'] {width: 100% !important;height: auto !important;}.font {font-size:18px !important;line-height:22px !important;}
+        .font1 {font-size:18px !important;line-height:22px !important;}}
+      </style>
+      <script type='colorScheme' class='swatch active'>
+        {'name':'Default','bgBody':'ffffff','link':'382F2E','color':'999999','bgItem':'ffffff','title':'222222'}
+      </script>
+      </head>
+        <body paddingwidth='0' paddingheight='0'   style='padding-top: 0; padding-bottom: 0; padding-top: 0; padding-bottom: 0; background-repeat: repeat; width: 100% !important; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%; -webkit-font-smoothing: antialiased;' offset='0' toppadding='0' leftpadding='0'>
+              <table bgcolor='#ffffff' width='100%' border='0' cellspacing='0' cellpadding='0' class='tableContent' align='center'  style='font-family:Helvetica, Arial,serif;'>
+            <tbody>
+              <tr>
+                <td><table width='600' border='0' cellspacing='0' cellpadding='0' align='center' bgcolor='#ffffff' class='MainContainer'>
+            <tbody>
+              <tr>
+                <td><table width='100%' border='0' cellspacing='0' cellpadding='0'>
+            <tbody>
+              <tr>
+                <td valign='top' width='40'>&nbsp;</td>
+                <td><table width='100%' border='0' cellspacing='0' cellpadding='0'>
+            <tbody>
+              <tr>
+                <td height='75' class='spechide'></td>
+              </tr>
+              <tr>
+                <td class='movableContentContainer ' valign='top'>
+                  <div class='movableContent' style='border: 0px; padding-top: 0px; position: relative;'>
+                    <table width='100%' border='0' cellspacing='0' cellpadding='0'>
+            <tbody>
+              <tr>
+                <td height='35'></td>
+              </tr>
+              <tr>
+                <td><table width='100%' border='0' cellspacing='0' cellpadding='0'>
+            <tbody>
+              <tr>
+                <td valign='top' align='center' class='specbundle'><div class='contentEditableContainer contentTextEditable'>
+                    <div class='contentEditable'>
+                      <p style='text-align:center;font-family:Georgia,Time,sans-serif;font-size:20px;color:#222222;'><span class='specbundle2'><span class='font1'>Trisakti Connect&nbsp;</span></span></p>
+                    </div>
+                  </div></td>
+                <td valign='top' class='specbundle'><div class='contentEditableContainer contentTextEditable'>
+                    <div class='contentEditable'>
+                      <p style='margin:0;font-family:Georgia,Time,sans-serif;font-size:26px;color:#69C374;'><span class='font'></span> </p>
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          </td>
+              </tr>
+            </tbody>
+          </table>
+              </div>
+              <div class='movableContent' style='border: 0px; padding-top: 0px; position: relative;'>
+                <table width='100%' border='0' cellspacing='0' cellpadding='0' align='center'>
+                  <tr>
+                    <td valign='top' align='center'>
+                      <div class='contentEditableContainer contentImageEditable'>
+                        <div class='contentEditable'>
+                          <img src='https://res.cloudinary.com/rendisimamora/image/upload/v1522947904/line_xp5hae.png' width='251' height='43' alt='' data-default='placeholder' data-max-width='560'>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+                </table>
+              </div>
+                <div class='movableContent' style='border: 0px; padding-top: 0px; position: relative;'>
+                <table width='100%' border='0' cellspacing='0' cellpadding='0' align='center'>
+                <tr>
+                  <td height='55'>
+                    <center><img src='http://res.cloudinary.com/rendisimamora/image/upload/v1522948034/logo-small-blue_hmvipx.png' width='100px' style='border-radius:100%'/></center>
+                  </td>
+                </tr>
+                    <tr>
+                      <td align='left'>
+                        <div class='contentEditableContainer contentTextEditable'>
+                          <div class='contentEditable' align='center'>
+                            <h2>Hai ".$x."</h2>
+                          </div>
+                        </div>
+                      </td>
+                    </tr>
+                    <tr><td height='15'> </td></tr>
+                  <tr>
+                    <td align='left'>
+                      <div class='contentEditableContainer contentTextEditable'>
+                        <div class='contentEditable' align='center'>
+                          <p >
+                            Selamat, Akun anda telah berhasil diverifikasi. Sekarang anda sudah terdaftar sebagai anggota IKAUSAKTI ( Ikatan Alumni Trisakti ). 
+                            <br>
+                            <br>
+                            Ada pertanyaan? Hubungi kami melalui email ke tim dukungan kami.
+                            <br>
+                            <br>
+                            Cheers,
+                            <br>
+                            <span style='color:#222222;'>Admin Trisakti Connect</span>
+
+                          </p>
+                        </div>
+                      </div>
+                    </td>
+                  </tr>
+              </table>
+            </div>
+                  <div class='movableContent' style='border: 0px; padding-top: 0px; position: relative;'>
+                    <table width='100%' border='0' cellspacing='0' cellpadding='0'>
+            <tbody>
+              <tr>
+                <td height='65'>
+              </tr>
+              <tr>
+                <td  style='border-bottom:1px solid #DDDDDD;'></td>
+              </tr>
+              <tr><td height='25'></td></tr>
+              <tr>
+                <td><table width='100%' border='0' cellspacing='0' cellpadding='0'>
+            <tbody>
+              <tr>
+                <td valign='top' class='specbundle'><div class='contentEditableContainer contentTextEditable'>
+                  <div class='contentEditable' align='center'>
+                    <p  style='text-align:left;color:#CCCCCC;font-size:12px;font-weight:normal;line-height:20px;'>
+                      <span style='font-weight:bold;'>Trisakti Connect</span>
+                      <br>
+                      Jalan Kyai Tapa No.1, RT.6/RW.16, Tomang, Grogol petamburan, Jakarta Barat, 11440
+                      <br>
+                    </p>
+                  </div>
+                </div></td>
+                <td valign='top' width='30' class='specbundle'>&nbsp;</td>
+                <td valign='top' class='specbundle'><table width='100%' border='0' cellspacing='0' cellpadding='0'>
+            <tbody>
+            </tbody>
+          </table>
+          </td>
+              </tr>
+            </tbody>
+          </table>
+          </td>
+              </tr>
+              <tr><td height='88'></td></tr>
+            </tbody>
+          </table>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          </td>
+                <td valign='top' width='40'>&nbsp;</td>
+              </tr>
+            </tbody>
+          </table>
+          </td>
+              </tr>
+            </tbody>
+          </table>
+          </td>
+              </tr>
+            </tbody>
+          </table>
+        </body>
+    </html>
+		");
+
+    $this->email->send(); 
+    $data = array(
+      'verified'     => 1,
 		);
+		
 		$where = array(
 			'id' => $id
-		);
-		$this->ModelDashboard->updatedata_cat($where,$data,'users');
-		redirect("registerusers");
-		
+    );
+    
+    $this->email->send(); 
+    $this->ModelDashboard->updatedata_cat($where,$data,'users');
+    redirect("registerusers");
 	}
-
 	// End Reports
 
 	public function sendreports()
